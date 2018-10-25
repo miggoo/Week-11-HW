c4bb24eb3950589ae113cb343f385a1e2af2806d
miggoo <michaelgrosvenor@gmail.com>
2018-10-24 20:26:58 -0700

    sub

---
+++ wk11hwflaskapp1.py
@@ -0,0 +1,55 @@
+### Routes
+
+* `/api/v1.0/precipitation`
+
+  * Query for the dates and temperature observations from the last year.
+
+  * Convert the query results to a Dictionary using `date` as the key and `tobs` as the value.
+
+  * Return the JSON representation of your dictionary.
+
+* `/api/v1.0/stations`
+
+  * Return a JSON list of stations from the dataset.
+
+* `/api/v1.0/tobs`
+
+  * Return a JSON list of Temperature Observations (tobs) for the previous year.
+
+* `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
+
+  * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+  * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+
+  * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+
+## Hints
+
+* You will need to join the station and measurement tables for some of the analysis queries.
+
+* Use Flask `jsonify` to convert your API data into a valid JSON response object.
+
+# 1. import Flask
+from flask import Flask
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/api/v1.0/precipitation")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+
+# 4. Define what to do when a user hits the /about route
+@app.route("/api/v1.0/tobs")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
